12daa429f9ba1cf23b9333ffa5ae9b8372e09933
befree1986 <g.marino787@gmail.com>
2025-08-03 20:26:20 +0200

    aggiornamento immagini

--- src/data/galleryData.ts
+++ src/data/galleryData.ts
@@ -134,10 +134,26 @@ export const galleryData: GallerySection[] = [
      },
     images: [
       {
-        src: '/cameraDoppia/2_giorni.jpg',
+        src: '/openSpace/open1.jpg',
         altKey: 'propriety.gallery.section4.img1.alt',
         
       },
+      {
+        src: '/openSpace/open2.jpg',
+        altKey: 'propriety.gallery.section4.img2.alt',
+      },
+      {
+        src: '/openSpace/open3.jpg',
+        altKey: 'propriety.gallery.section4.img3.alt',
+      },
+      {
+        src: '/openSpace/open4a.jpg',
+        altKey: 'propriety.gallery.section4.img4.alt',
+      },
+      {
+        src: '/openSpace/open4.jpg',
+        altKey: 'propriety.gallery.section4.img5.alt',
+      }
     
     ]
   },
@@ -152,6 +168,46 @@ export const galleryData: GallerySection[] = [
         src: '/bagno1/bagno1.jpg',
         altKey: 'propriety.gallery.section5.img1.alt',  
       },
+      {
+        src: '/bagno1/bagno2.jpg',
+        altKey: 'propriety.gallery.section5.img2.alt',
+      },
+      {
+        src: '/bagno1/bagno3.jpg',
+        altKey: 'propriety.gallery.section5.img3.alt',
+      },
+      {
+        src: '/bagno2/bagno4a.jpg',
+        altKey: 'propriety.gallery.section5.img4.alt',
+      },
+      {
+        src:'/bagno2/bagno4.jpg',
+        altKey: 'propriety.gallery.section5.img5.alt',
+      },
+      {
+        src: '/bagno2/bagno5.jpg',
+        altKey: 'propriety.gallery.section5.img6.alt',
+      },
+      {
+        src: '/bagno2/bagno6.jpg',
+        altKey: 'propriety.gallery.section5.img7.alt',
+      },
+      {
+        src: '/bagno2/bagno7.jpg',
+        altKey: 'propriety.gallery.section5.img8.alt',
+      },
+      {
+        src: '/bagno2/bagno7.jpg',
+        altKey: 'propriety.gallery.section5.img9.alt',
+      },
+      {
+        src: '/bagno2/bagno8.jpg',
+        altKey: 'propriety.gallery.section5.img10.alt',
+      },
+      {
+        src: '/bagno2/bagno9.jpg',
+        altKey: 'propriety.gallery.section5.img11.alt',
+      }
     ]
   },
 
@@ -162,9 +218,46 @@ export const galleryData: GallerySection[] = [
     },
     images: [
       {
-        src: '/bagno2/bagno2.jpg',
+        src: '/esterni/esterno1.jpg',
         altKey: 'propriety.gallery.section6.img1.alt',
       },
+      {
+        src: '/esterni/esterno2.jpg',
+        altKey: 'propriety.gallery.section6.img2.alt',
+      },
+      {
+        src: '/esterni/esterno3.jpg',
+        altKey: 'propriety.gallery.section6.img3.alt',
+      },
+      {
+        src: '/esterni/esterno4.jpg',
+        altKey: 'propriety.gallery.section6.img4.alt',
+      },
+      {
+        src: '/esterni/esterno5.jpg',
+        altKey: 'propriety.gallery.section6.img5.alt',
+      },
+
+    {
+      src: '/esterni/esterno6.jpg',
+      altKey: 'propriety.gallery.section6.img6.alt',
+    },
+    {
+      src: '/esterni/esterno7.jpg',
+      altKey: 'propriety.gallery.section6.img7.alt',
+    },
+    {
+      src: '/esterni/esterno8.jpg',
+      altKey: 'propriety.gallery.section6.img8.alt',
+    },
+    {
+      src: '/esterni/esterno9.jpg',
+      altKey: 'propriety.gallery.section6.img9.alt',
+    },
+    {
+      src: '/esterni/esterno10.jpg',
+      altKey: 'propriety.gallery.section6.img10.alt',
+    }
     ]
   }
 
